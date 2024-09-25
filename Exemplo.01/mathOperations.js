5f6d9733c462426e137c7b4ec5de1919da70237e
IgorPeneluc <150184067+IgorPeneluc@users.noreply.github.com>
2024-09-24 21:48:10 -0300

    Aula feita

---
+++ Exemplo.01/mathOperations.js
@@ -0,0 +1,16 @@
+// mathOperations.js
+
+function add(a, b) {
+
+    return a + b;
+
+}
+
+ function subtract(a, b) {
+
+    return a - b;
+
+}
+
+ module.exports = { add, subtract };
+
---
+++ Exemplo05/Server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+ 
+app.use(cors());
+ 
+let items = ['Item 1', 'Item 2', 'Item 3'];
+ 
+app.get('/api/items', (req, res) => {
+  res.json(items);
+});
+ 
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
